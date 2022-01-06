ec7b274726c700303560e69e1d1901e0600316a3
tburke <tylerburke@live.com.au>
2022-01-06 19:53:28 +1100

    ci(commitlint): remove body limit length

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "footer-max-line-length": [2, "always", Infinity],
+    "body-max-line-length": [2, "always", Infinity],
   },
 };
