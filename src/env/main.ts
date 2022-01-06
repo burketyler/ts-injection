3194bb9c59f4bb4e831a6c90cb57f43daddf47b0
tburke <tylerburke@live.com.au>
2022-01-06 11:24:44 +1100

    feat(env): refactor to new structure, improve code
    BREAKING CHANGE:
    wont throw on missing var, new options object

--- src/env/main.ts
+++ src/env/main.ts
@@ -1,60 +1,81 @@
-import "reflect-metadata";
+import { PRIMITIVE_TYPES } from "../constants";
 import { Debugger } from "../debugger";
-import { PrimitiveType } from "../types/primitive-type";
-import { extractType } from "./utils";
+import { Newable } from "../types";
+
+import { Options, EnvironmentVariableError } from "./types";
 
 const logger: Debugger = Debugger.getInstance("Env");
 
-export function Env<T>(varName: string, mapper?: (val: string) => T): any {
-  if (process && process.env) {
-    return (classCtor: any, fieldName: string) => {
-      const type = extractType(classCtor, fieldName);
-      logger.debug(
-        `Injecting env var ${varName} into ${classCtor.constructor.name}.${fieldName}`
-      );
-      injectVarIntoMember(
-        type,
-        classCtor,
-        fieldName,
-        process.env[varName],
-        mapper
-      );
-    };
-  } else {
-    throw new Error("The environment doesn't have process or process.env!");
+export function Env<VariableType>(
+  varName: string,
+  options: Options<VariableType>
+) {
+  if (!process || !process.env) {
+    throw new EnvironmentVariableError(
+      "Invalid runtime, process or process.env is undefined."
+    );
   }
+
+  return (classCtor: Newable, fieldName: string) => {
+    const type = Reflect.getMetadata("design:type", classCtor, fieldName);
+
+    logger.debug(
+      `Injecting env var ${varName} into ${classCtor.constructor.name}.${fieldName}`
+    );
+
+    injectIntoField(
+      type,
+      classCtor,
+      varName,
+      fieldName,
+      process.env[varName],
+      options
+    );
+  };
 }
 
-function injectVarIntoMember(
+function injectIntoField(
   type: () => any,
   classCtor: any,
+  varName: string,
   fieldName: string,
-  envVar: string,
-  mapper?: (val: string) => any
+  envVar: string | undefined,
+  { mapper, failBehaviour }: Options<unknown>
 ): void {
   if (!envVar) {
-    throw new Error(`Environment variable is undefined.`);
+    const message = `Environment variable '${varName}' is undefined.`;
+    console.warn(message); // eslint-disable-line no-console
+
+    if (failBehaviour === "THROW") {
+      throw new EnvironmentVariableError(message);
+    }
+
+    return;
   }
+
   logger.debug(`Variable resolved to ${envVar}.`);
+
   if (mapper) {
-    logger.debug("Using custom mapper to parse var.");
-    classCtor[fieldName] = mapper(envVar);
+    logger.debug("Using provided mapper to parse.");
+    classCtor[fieldName] = mapper(envVar); // eslint-disable-line no-param-reassign
   } else {
-    logger.debug(`Using provided type ${type} to parse var.`);
-    classCtor[fieldName] = parseEnvVarByType(type, envVar);
+    logger.debug(`Using inferred type ${type} to parse.`);
+    classCtor[fieldName] = parseEnvVarByInferredType(type, envVar); // eslint-disable-line no-param-reassign
   }
 }
 
-function parseEnvVarByType(
-  type: () => any,
+function parseEnvVarByInferredType(
+  type: () => unknown,
   envVar: string
-): number | boolean | object | string {
+): number | boolean | Record<string, unknown> | string {
+  const { NUMBER, BOOLEAN, OBJECT } = PRIMITIVE_TYPES;
+
   switch (type) {
-    case PrimitiveType.NUMBER:
+    case NUMBER:
       return Number(envVar);
-    case PrimitiveType.BOOLEAN:
+    case BOOLEAN:
       return envVar === "true";
-    case PrimitiveType.OBJECT:
+    case OBJECT:
       return JSON.parse(envVar);
     default:
       return envVar;
